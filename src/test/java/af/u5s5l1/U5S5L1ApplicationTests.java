7f01fe93ea28558cdf143e4f769ecedd0c9789d2
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-30 13:22:42 +0100

    project initialization

---
+++ src/test/java/af/u5s5l1/U5S5L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package af.u5s5l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S5L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
