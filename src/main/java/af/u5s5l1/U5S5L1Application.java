7eb42be3d2ab1d8ca447ed3c5fcf16f5c8e5a328
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-30 15:53:10 +0100

    Update U5S5L1Application.java

--- src/main/java/af/u5s5l1/U5S5L1Application.java
+++ src/main/java/af/u5s5l1/U5S5L1Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class U5S5L1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(U5S5L1Application.class, args);
+
+
+
 	}
 
 }
